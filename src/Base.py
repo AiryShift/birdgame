006cc4bd06a9f5033101ea01e3fd048d9dc87731
AiryShift <religionofaperturelabs@gmail.com>
2016-05-10 05:30:49 +1000

    Add additional methods to Vector, some unittests

--- src/Base.py
+++ src/Base.py
@@ -3,39 +3,77 @@ import math
 
 
 class Vector():
-    def __init__(self, x=1, y=0):
-        self._vec = complex(x, y)
+    def __init__(self, *, x=1, y=0, arg=None):
+        if arg is not None:
+            self.vec = cmath.rect(1, arg)
+        else:
+            self.vec = complex(x, y)
 
     @property
     def x(self):
-        return self._vec.real
-    
+        return self.vec.real
+
     @x.setter
     def x(self, value):
-        self._vec.real = value
+        self.vec.real = value
 
     @property
     def y(self):
-        return self._vec.imag
+        return self.vec.imag
 
     @y.setter
     def y(self, value):
-        self._vec.imag = value
+        self.vec.imag = value
 
     def __add__(self, other):
-        return Vector(self.x + other.x, self.y + other.y)
+        if isinstance(other, Vector):
+            other = other.vec
+        elif not (isinstance(other, int) or isinstance(other, float)):
+            raise TypeError('Vector does not support {} with type {}'.format(
+                'addition', type(other)))
+        result = self.vec + other
+        return Vector(x=result.real, y=result.imag)
 
+    def __mul__(self, other):
+        if isinstance(other, Vector):
+            other = other.vec
+        elif not (isinstance(other, int) or isinstance(other, float)):
+            raise TypeError('Vector does not support {} with type {}'.format(
+                'multiplication', type(other)))
+        result = self.vec * other
+        return Vector(x=result.real, y=result.imag)
+
+    __rmul__ = __mul__
+
+    def __abs__(self):
+        return abs(self.vec)
+
+    def __eq__(self, other):
+        if isinstance(other, Vector):
+            other = other.vec
+        elif not (isinstance(other, int) or isinstance(other, float)):
+            raise TypeError('Vector does not support {} with type {}'.format(
+                'equality', type(other)))
+        return self.vec == other
 
 
 class PhysicalObject():
-    def __init__(self, position, velocity, acceleration, elasticity):
+    def __init__(self, position, velocity, acceleration, elasticity, mass):
         self.position = position
         self.velocity = velocity
         self.acceleration = acceleration
         self.elasticity = elasticity
+        self.mass = mass
 
     def move(self):
-        return NotImplementedError
+        self.velocity += self.acceleration
+        self.position += self.velocity
 
 if __name__ == '__main__':
-    pass
+    assert(Vector(arg=0) == Vector(x=1, y=0))
+    try:
+        Vector(arg=0) + 'a'
+    except TypeError:
+        pass
+    assert(Vector(arg=0) * 2 == Vector(x=2))
+    assert(2 * Vector(arg=0) == Vector(x=2))
