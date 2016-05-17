f3c1be529fadf53d6aafb99d7e85bd53fd3000c4
AiryShift <religionofaperturelabs@gmail.com>
2016-05-17 15:02:26 +1000

    Add PhysicalObject tests

--- birdgame/tests.py
+++ birdgame/tests.py
@@ -1,26 +1,49 @@
 import unittest
 import src.obj.ObjBase as ObjBase
+from src.obj.ObjBase import Vector
 import src.obj.Wall as Wall
 
 
 class TestVector(unittest.TestCase):
     def test_operators(self):
-        self.assertEqual(ObjBase.Vector(arg=0), ObjBase.Vector(x=1, y=0))
-        self.assertEqual(ObjBase.Vector(arg=0) * 2, ObjBase.Vector(x=2))
-        self.assertEqual(2 * ObjBase.Vector(arg=0), ObjBase.Vector(x=2))
+        self.assertEqual(Vector(arg=0), Vector(x=1, y=0))
+        self.assertEqual(Vector(arg=0) * 2, Vector(x=2))
+        self.assertEqual(2 * Vector(arg=0), Vector(x=2))
         with self.assertRaises(TypeError):
-            ObjBase.Vector(arg=0) + 'a'
+            Vector(arg=0) + 'a'
 
 
 class TestPhysicalObject(unittest.TestCase):
+    def setUp(self):
+        self.same_1 = ObjBase.PhysicalObject(
+            Vector(x=0, y=0), Vector(x=2, y=2),
+            Vector(x=0, y=0), Vector(x=0, y=0),
+            0, 0)
+        self.same_2 = ObjBase.PhysicalObject(
+            Vector(x=0, y=0), Vector(x=2, y=2),
+            Vector(x=0, y=0), Vector(x=0, y=0),
+            0, 0)
+        self.moving = ObjBase.PhysicalObject(
+            Vector(x=4, y=0), Vector(x=2, y=2),
+            Vector(x=-1, y=0), Vector(x=0, y=0),
+            0, 0)
+
     def test_detect_collision(self):
-        pass
+        self.assertTrue(self.same_1.detect_collision(self.same_2))
+        self.assertTrue(self.same_2.detect_collision(self.same_1))
+
+        self.assertFalse(self.same_1.detect_collision(self.moving))
+        self.moving.move()
+        self.assertFalse(self.same_1.detect_collision(self.moving))
+        self.moving.move()
+        self.assertTrue(self.same_1.detect_collision(self.moving))
+        self.assertTrue(self.same_2.detect_collision(self.moving))
 
 
 class TestWalls(unittest.TestCase):
     def test_move(self):
-        with self.assertRaises(TypeError):
-            Wall().move()
+        with self.assertRaises(ObjBase.PhysicalError):
+            Wall.Wall().move()
 
 if __name__ == '__main__':
     unittest.main(verbosity=1)
