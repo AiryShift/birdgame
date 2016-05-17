46f291d56abeeca1e120043533f334f33c8ff0e1
AiryShift <religionofaperturelabs@gmail.com>
2016-05-18 09:38:18 +1000

    Implement Walls

--- birdgame/src/obj/Wall.py
+++ birdgame/src/obj/Wall.py
@@ -2,8 +2,8 @@ from . import ObjBase
 
 
 class Wall(ObjBase.PhysicalObject):
-    def __init__(self):
-        pass
+    def __init__(self, position, size):
+        super().__init__(position, size, None, None, None, None)
 
     def move(self):
         raise ObjBase.PhysicalError('Walls do not move')
