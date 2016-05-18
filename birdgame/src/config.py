6e126e4d094ca2ea548ff45741577c43c320e652
AiryShift <religionofaperturelabs@gmail.com>
2016-05-18 11:09:38 +1000

    Add config/defaults file

---
+++ birdgame/src/config.py
@@ -0,0 +1,15 @@
+# Colors
+WHITE = (255, 255, 255)
+
+# Different screens
+EXIT_CODE = -1
+MAIN_MENU_CODE = 0
+CONTROLS_CODE = 1
+SELECT_CODE = 2
+GAME_CODE = 3
+
+BIRD_MASS = 10  # temporary
+BIRD_ELASTICITY = 0
+
+BALL_MASS = 10  # temporary
+BALL_ELASTICITY = 5  # temporary
