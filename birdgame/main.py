2626b8baf6c7a1d75f3631caf0d59c500790e75f
AiryShift <religionofaperturelabs@gmail.com>
2016-05-19 00:07:51 +1000

    Remove unnecessary quit
    Main uses default values to start on

--- birdgame/main.py
+++ birdgame/main.py
@@ -2,16 +2,12 @@ import src.screens.MainMenu as MainMenu
 import src.screens.Controls as Controls
 import src.screens.Select as Select
 import src.screens.Game as Game
-import pygame as pg
-
+import src.config as config
 
 if __name__ == '__main__':
-    pg.init()
-    clock = pg.time.Clock()
-
-    # The ordering must follow the return codes in config with exit last
-    states = [MainMenu.run, Controls.run, Select.run, Game.run, pg.exit]
+    # The ordering must follow the return codes in config
+    states = [MainMenu.run, Controls.run, Select.run, Game.run]
     settings = {}
-    return_code = 0
-    while True:
+    return_code = config.DEFAULT_SCREEN_CODE
+    while return_code != config.EXIT_CODE:
         return_code = states[return_code](settings)
