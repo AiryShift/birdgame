56c6170482f16ccd5463968a4e7e6952a1911fcc
AiryShift <religionofaperturelabs@gmail.com>
2016-05-18 21:58:51 +1000

    Use some of those return codes

--- birdgame/main.py
+++ birdgame/main.py
@@ -1,2 +1,17 @@
-import src.obj as obj
-import src.screens as screens
+import src.screens.MainMenu as MainMenu
+import src.screens.Controls as Controls
+import src.screens.Select as Select
+import src.screens.Game as Game
+import pygame as pg
+
+
+if __name__ == '__main__':
+    pg.init()
+    clock = pg.time.Clock()
+
+    # The ordering must follow the return codes in config with exit last
+    states = [MainMenu.run, Controls.run, Select.run, Game.run, pg.exit]
+    settings = {}
+    return_code = 0
+    while True:
+        return_code = states[return_code](settings)
