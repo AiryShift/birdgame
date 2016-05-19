609c9d79220a3947265c2a9f8de7ec88d81d5712
AiryShift <religionofaperturelabs@gmail.com>
2016-05-19 19:27:44 +1000

    Move bird defaults to default

--- birdgame/src/obj/Bird.py
+++ birdgame/src/obj/Bird.py
@@ -1,16 +1,16 @@
 from . import ObjBase
-BIRD_MASS = 10  # temporary
-BIRD_ELASTICITY = 0
+from .. import config
 BIRD_SIZE = ObjBase.Vector(x=10, y=10)  # temporary
 
 
 class Bird(ObjBase.PhysicalObject):
     def __init__(
-            self, position, size=BIRD_SIZE, velocity=0, acceleration=0,
-            elasticity=BIRD_ELASTICITY, mass=BIRD_MASS, orientation=None,
-            has_ball=False):
+            self, position, team, size=config.BIRD_SIZE, velocity=0,
+            acceleration=0, elasticity=config.BIRD_ELASTICITY,
+            mass=config.BIRD_MASS, orientation=None, has_ball=False):
         super().__init__(position, size, velocity, acceleration, elasticity,
                          mass)
         if orientation is None:
             self.orientation = ObjBase.Vector(arg=0)
+        self.team = team
         self.has_ball = has_ball
