a9cf033bdbb43f43cb3e8c94730f4d4fd73c7d21
AiryShift <religionofaperturelabs@gmail.com>
2016-05-20 23:12:03 +1000

    Add NotImplementedError to Bird

--- birdgame/src/obj/Bird.py
+++ birdgame/src/obj/Bird.py
@@ -1,15 +1,15 @@
 from src.Vector import Vector
 from src.config import config
 from . import ObjBase
-BIRD_SIZE = Vector(x=10, y=10)  # temporary
 
 
 class Bird(ObjBase.PhysicalObject):
     def __init__(
-            self, position, team, size=config.BIRD_SIZE, velocity=0,
+            self, position, team, image, velocity=0,
             acceleration=0, elasticity=config.BIRD_ELASTICITY,
             mass=config.BIRD_MASS, orientation=None, has_ball=False):
-        super().__init__(position, size, velocity, acceleration, elasticity,
+        raise NotImplementedError
+        super().__init__(position, None, velocity, acceleration, elasticity,
                          mass)
         if orientation is None:
             self.orientation = Vector(arg=0)
