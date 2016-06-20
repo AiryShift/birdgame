6cb81b1df0379038595524beb58b2d956c493eee
AiryShift <religionofaperturelabs@gmail.com>
2016-06-20 22:32:17 +1000

    Add argument parser to select default screen

--- birdgame/main.py
+++ birdgame/main.py
@@ -1,11 +1,20 @@
+import pygame as pg
 import src.screens.MainMenu as MainMenu
 import src.screens.Controls as Controls
 import src.screens.Select as Select
 import src.screens.Game as Game
 import src.config as cfg
-import pygame as pg
+import argparse
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser('Plays a game')
+    parser.add_argument('-d', '--debug',
+                        type=int,
+                        default=0,
+                        help='tarts game on screen with specified screen code')
+    args = parser.parse_args()
+    cfg.DEFAULT_SCREEN_CODE = args.debug
+
     pg.init()
     # The ordering must follow the return codes in cfg
     # Each state must implement a function run(screen, settings)
