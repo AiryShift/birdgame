807a9814dc7ad4114c178d7670df4a9575a11ce3
AiryShift <religionofaperturelabs@gmail.com>
2017-10-31 00:40:50 +1100

    Remove all this old code

--- birdgame/src/screens/Game.py
+++
@@ -1,8 +0,0 @@
-import pygame as pg
-import src.config as cfg
-from src.Vector import Vector
-from src.obj.Wall import Wall
-
-
-def run(screen, settings):
-    return cfg.EXIT_CODE
--- birdgame/src/screens/Select.py
+++
@@ -1,6 +0,0 @@
-import pygame as pg
-import src.config as cfg
-
-
-def run(screen, settings):
-    return cfg.EXIT_CODE
