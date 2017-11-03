7da9022acbea0467ea6184f4bf8806648349f1b2
AiryShift <religionofaperturelabs@gmail.com>
2017-11-04 04:10:01 +1100

    controller: implement run

--- src/controller.py
+++ src/controller.py
@@ -1,4 +1,5 @@
 import pygame as pg
+from views.game_view import GameView
 
 class Controller:
     def __init__(self, config, screen, clock):
@@ -6,5 +7,11 @@ class Controller:
         self.screen = screen
         self.clock = clock
 
+        view_list = [view(config, screen, clock) for view in [GameView]]
+        self.views = {view.name: view for view in view_list}
+
     def run(self):
-        pass
+        transition = 'game'  # default first view, TODO: something more customisable
+        while True:
+            next_view = self.views[transition]
+            transition = next_view.render()
