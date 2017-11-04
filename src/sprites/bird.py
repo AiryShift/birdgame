e972f4fd107e64680ddf6e02b068718b7f446740
AiryShift <religionofaperturelabs@gmail.com>
2017-11-04 18:39:09 +1100

    bird: implement bird rotation

--- src/sprites/bird.py
+++ src/sprites/bird.py
@@ -1,5 +1,11 @@
+import enum
 import pygame as pg
 from sprites.sprite import AbstractPhysicsSprite
+
+
+class Rotation(enum.Enum):
+    CLOCKWISE = enum.auto()
+    ANTICLOCKWISE = enum.auto()
 
 
 class Bird(AbstractPhysicsSprite):
@@ -8,7 +14,25 @@ class Bird(AbstractPhysicsSprite):
         image.fill(pg.Color('RED'))
         super().__init__(config, image)
 
-        self.direction = 0
+        # orientation in degrees, 0 east, positive anticlockwise
+        self.orientation = 0
 
     def move(self):
         super().move()
+
+    def turn(self, rotation):
+        if rotation is Rotation.CLOCKWISE:
+            self.orientation -= 5  # FIXME: better value
+        else:
+            self.orientation += 5  # FIXME: better value
+        self.orientation = Bird._normalise_orientation(orientation)
+
+    @staticmethod
+    def _normalise_orientation(orientation):
+        if orientation > 0:
+            while orientation > 360:
+                orientation -= 360
+        elif orientation < 0:
+            while orientation < 0:
+                orientation += 360
+        return orientation
