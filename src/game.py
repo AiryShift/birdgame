647f851ce284f88e97364538b805cd9740178425
AiryShift <religionofaperturelabs@gmail.com>
2017-11-25 00:25:05 +1100

    project: swap LICENSE to Apache
    Patenting reasons. Don't wanna sue/get sued.
    
    Resolves #6

--- src/game.py
+++ src/game.py
@@ -1,13 +1,28 @@
 #! /usr/bin/env python
 
+# Copyright 2017 Julian Tu
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from controller import Controller
 import json
+from json_minify import json_minify
 import pygame as pg
 
 
 if __name__ == '__main__':
     with open('config.json') as json_file:
-        config = json.loads(json_file.read())
+        config = json.loads(json_minify(json_file.read()))
 
     pg.init()
     screen = pg.display.set_mode(
